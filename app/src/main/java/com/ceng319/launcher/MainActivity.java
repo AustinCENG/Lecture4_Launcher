38d7c5af3fefff98184d4ecdbffa54e1a1d05227
Austin <tianyuxin@gmail.com>
2019-09-25 20:56:26 -0400

    Initial commit

---
+++ app/src/main/java/com/ceng319/launcher/MainActivity.java
@@ -0,0 +1,46 @@
+package com.ceng319.launcher;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO 3: Set up the Theme with Launch Theme in onCreate method.
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
---
+++ app/src/main/java/com/ceng319/launcher/Readme.txt
@@ -0,0 +1,6 @@
+Please refer to this website for how to correctly setup the launcher screen.
+https://android.jlelse.eu/the-complete-android-splash-screen-guide-c7db82bce565
+
+Follow the first option Using a Launcher Theme to setup the launcher screen(splash screen) for the APP
+
+Follow the "TODO" list in this APP to change your APP so that you could have the splash screen designed. 
